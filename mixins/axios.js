b8219fc6a569161acbbe2172a2dd8341ccbeb251
victor.zabala <victor.zabala@interedes.com.co>
2021-09-16 09:54:16 -0500

    axios module

--- mixins/axios.js
+++ mixins/axios.js
@@ -7,6 +7,46 @@ const axios = {
             } catch (error) {
                 callback(error.response)
             }
+        },
+        async $getOne(endpoint, id, callback) {
+            try {
+                const res = await this.$axios.get(`${endpoint}/${id}`)
+                callback(res)
+            } catch (error) {
+                callback(error.response)
+            }
+        },
+        async $post(endpoint, object, callback) {
+            try {
+                const res = await this.$axios.post(endpoint, object)
+                callback(res)
+            } catch (error) {
+                callback(error.response)
+            }
+        },
+        async $put(endpoint, id, object, callback) {
+            try {
+                const res = await this.$axios.put(`${endpoint}/${id}`, object)
+                callback(res)
+            } catch (error) {
+                callback(error.response)
+            }
+        },
+        async $delete(endpoint, id, callback) {
+            try {
+                const res = await this.$axios.delete(`${endpoint}/${id}`)
+                callback(res)
+            } catch (error) {
+                callback(error.response)
+            }
+        },
+        async $patch(endpoint, id, object, callback) {
+            try {
+                const res = await this.$axios.patch(`${endpoint}/${id}`, object)
+                callback(res)
+            } catch (error) {
+                callback(error.response)
+            }
         }
     }
 }
